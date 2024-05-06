49bf70814852d1172afe8fcb1ddd73e4986da934
vanshita00 <129608607+vanshita00@users.noreply.github.com>
2024-05-06 18:34:05 +0530

    Add files via upload

---
+++ stripePayment/src/main/java/com/stripePayment/config/Config.java
@@ -0,0 +1,19 @@
+package com.stripePayment.config;
+
+
+import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    @Value("${stripe.api.secretKey}")
+    private String secretKey;
+
+    @PostConstruct
+    public void  initSecretKey(){
+        Stripe.apiKey = secretKey;
+    }
+}
+
